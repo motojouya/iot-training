77702a0cbbc6009c7971c78dc417009ec9fda5c9
Takuya Sugiyama (@motojouya) <motojouya@gmail.com>
2024-01-05 13:39:42 +0000

    glue table

--- data_warehouse/glue.tf
+++ data_warehouse/glue.tf
@@ -1,38 +1,32 @@
 
-variable "glue_catalog_database_name" {}
-variable "glue_catalog_table_name" {}
-variable "date_range_start" {}
-variable "data_bucket_name" {}
-variable "data_prefix" {} # /home/air
-
-
-
 resource "aws_glue_catalog_database" "home_air_database" {
   name = var.glue_catalog_database_name
 }
 
 resource "aws_glue_catalog_table" "home_air_table" {
-  database_name = aws_glue_catalog_database.iot_database.name
+  database_name = aws_glue_catalog_database.home_air_database.name
   name          = var.glue_catalog_table_name
 
   table_type = "EXTERNAL_TABLE"
 
   parameters = {
-    "projection.enabled"                 = "true"
-    "projection.orderdate.format"        = "yyyy/MM/dd"
-    "projection.orderdate.type"          = "date"
-    "projection.orderdate.interval"      = "1"
-    "projection.orderdate.interval.unit" = "DAYS"
-    "projection.orderdate.range"         = "${var.date_range_start},NOW"
-    "storage.location.template"          = "s3://${var.data_bucket_name}${var.data_prefix}/$${time}"
+    "projection.enabled"            = "true"
+    "projection.time.format"        = "yyyy/MM/dd"
+    "projection.time.type"          = "date"
+    "projection.time.interval"      = "1"
+    "projection.time.interval.unit" = "DAYS"
+    "projection.time.range"         = "${var.date_range_start},NOW"
+    "storage.location.template"     = "s3://${var.data_bucket_name}${var.data_prefix}/$${time}"
+    "classification"                = "orc"
   }
 
   storage_descriptor {
     location      = "s3://${var.data_bucket_name}${var.data_prefix}"
-    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
-    output_format = "org.apache.hadoop.hive.ql.io.IgnoreKeyTextOutputFormat"
+    input_format  = "org.apache.hadoop.hive.ql.io.orc.OrcInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.orc.OrcOutputFormat"
+    compressed = true
     ser_de_info {
-      serialization_library = "org.openx.data.jsonserde.JsonSerDe"
+      serialization_library = "org.apache.hadoop.hive.ql.io.orc.OrcSerde"
       parameters = {
         "serialization.format" = "1"
       }
--- data_warehouse/variable.tf
+++ data_warehouse/variable.tf
@@ -6,4 +6,8 @@ variable "athena_bucket_name" {}
 variable "general_work_group_name" {}
 
 # glue
-
+variable "glue_catalog_database_name" {}
+variable "glue_catalog_table_name" {}
+variable "date_range_start" {}
+variable "data_bucket_name" {}
+variable "data_prefix" {}

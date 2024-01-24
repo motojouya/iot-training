65c8483b08d4ae6ae2a6f32470f3748a12ecfbf3
motojouya <motojouya@gmail.com>
2024-01-24 23:14:26 +0900

    iot role

--- input_stream/iot_topic_rule.tf
+++ input_stream/iot_topic_rule.tf
@@ -1,5 +1,71 @@
 resource "aws_cloudwatch_log_group" "iot_logs" {
-  name = var.error_log # motojouya/iot/logs
+  name = var.error_log
+}
+
+data "aws_iam_policy_document" "iot_policy_data" {
+
+  statement {
+    actions = [
+      "logs:GetLogDelivery",
+      "logs:ListLogDeliveries",
+      "logs:DeleteAccountPolicy",
+      "logs:DeleteResourcePolicy",
+      "logs:StopLiveTail",
+      "logs:CancelExportTask",
+      "logs:DeleteLogDelivery",
+      "logs:DescribeQueryDefinitions",
+      "logs:DescribeResourcePolicies",
+      "logs:DescribeDestinations",
+      "logs:DescribeQueries",
+      "logs:DescribeLogGroups",
+      "logs:DescribeAccountPolicies",
+      "logs:DescribeDeliverySources",
+      "logs:StopQuery",
+      "logs:TestMetricFilter",
+      "logs:DeleteQueryDefinition",
+      "logs:PutQueryDefinition",
+      "logs:PutAccountPolicy",
+      "logs:DescribeDeliveryDestinations",
+      "logs:Link",
+      "logs:CreateLogDelivery",
+      "logs:PutResourcePolicy",
+      "logs:DescribeExportTasks",
+      "logs:UpdateLogDelivery",
+      "firehose:ListDeliveryStreams",
+      "logs:DescribeDeliveries"
+    ]
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "logs:*"
+    ]
+    resources = [
+      "arn:aws:logs:*:${var.account_id}:log-group:",
+    ]
+  }
+
+  statement {
+    actions = [
+      "firehose:DescribeDeliveryStream",
+      "firehose:PutRecord",
+      "firehose:PutRecordBatch",
+      "firehose:ListTagsForDeliveryStream",
+      "firehose:TagDeliveryStream",
+      "firehose:UntagDeliveryStream"
+    ]
+    resources = [
+      "arn:aws:firehose:*:${var.account_id}:deliverystream/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "iot_policy" {
+  name   = "terraform_iot_policy"
+  policy = data.aws_iam_policy_document.iot_policy_data.json
 }
 
 data "aws_iam_policy_document" "iot_assume_role" {
@@ -18,6 +84,11 @@ data "aws_iam_policy_document" "iot_assume_role" {
 resource "aws_iam_role" "iot_role" {
   name               = "iot_role"
   assume_role_policy = data.aws_iam_policy_document.iot_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "iot_policy_attachment" {
+  role       = aws_iam_role.iot_role.name
+  policy_arn = aws_iam_policy.iot_policy.arn
 }
 
 resource "aws_iot_topic_rule" "iot_topic_rule" {
--- input_stream/things_policy.tf
+++ input_stream/things_policy.tf
@@ -1,49 +1,58 @@
 data "aws_iam_policy_document" "things_policy_doc" {
   statement {
     actions = [
-      "iot:Connect",
+      "*",
     ]
     resources = [
-      "arn:aws:iot:ap-northeast-1:${var.account_id}:client/&{iot:Connection.Thing.ThingName}",
+      "*",
     ]
   }
 
-  statement {
-    actions = [
-      "iot:Publish",
-    ]
-    resources = [
-      "arn:aws:iot:ap-northeast-1:${var.account_id}:topic/data/&{iot:Connection.Thing.ThingName}",
-      "arn:aws:iot:ap-northeast-1:${var.account_id}:topic/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/update",
-      "arn:aws:iot:ap-northeast-1:${var.account_id}:topic/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/get"
-    ]
-  }
+  # statement {
+  #   actions = [
+  #     "iot:Connect",
+  #   ]
+  #   resources = [
+  #     "arn:aws:iot:ap-northeast-1:${var.account_id}:client/&{iot:Connection.Thing.ThingName}",
+  #   ]
+  # }
 
-  statement {
-    actions = [
-      "iot:Receive",
-    ]
-    resources = [
-      "arn:aws:iot:ap-northeast-1:${var.account_id}:topic/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/update/delta",
-      "arn:aws:iot:ap-northeast-1:${var.account_id}:topic/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/update/accepted",
-      "arn:aws:iot:ap-northeast-1:${var.account_id}:topic/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/update/rejected",
-      "arn:aws:iot:ap-northeast-1:${var.account_id}:topic/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/get/accepted",
-      "arn:aws:iot:ap-northeast-1:${var.account_id}:topic/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/get/rejected"
-    ]
-  }
+  # statement {
+  #   actions = [
+  #     "iot:Publish",
+  #   ]
+  #   resources = [
+  #     "arn:aws:iot:ap-northeast-1:${var.account_id}:topic/data/&{iot:Connection.Thing.ThingName}",
+  #     "arn:aws:iot:ap-northeast-1:${var.account_id}:topic/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/update",
+  #     "arn:aws:iot:ap-northeast-1:${var.account_id}:topic/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/get"
+  #   ]
+  # }
 
-  statement {
-    actions = [
-      "iot:Subscribe",
-    ]
-    resources = [
-      "arn:aws:iot:ap-northeast-1:${var.account_id}:topicfilter/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/update/delta",
-      "arn:aws:iot:ap-northeast-1:${var.account_id}:topicfilter/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/update/accepted",
-      "arn:aws:iot:ap-northeast-1:${var.account_id}:topicfilter/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/update/rejected",
-      "arn:aws:iot:ap-northeast-1:${var.account_id}:topicfilter/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/get/accepted",
-      "arn:aws:iot:ap-northeast-1:${var.account_id}:topicfilter/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/get/rejected"
-    ]
-  }
+  # statement {
+  #   actions = [
+  #     "iot:Receive",
+  #   ]
+  #   resources = [
+  #     "arn:aws:iot:ap-northeast-1:${var.account_id}:topic/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/update/delta",
+  #     "arn:aws:iot:ap-northeast-1:${var.account_id}:topic/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/update/accepted",
+  #     "arn:aws:iot:ap-northeast-1:${var.account_id}:topic/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/update/rejected",
+  #     "arn:aws:iot:ap-northeast-1:${var.account_id}:topic/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/get/accepted",
+  #     "arn:aws:iot:ap-northeast-1:${var.account_id}:topic/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/get/rejected"
+  #   ]
+  # }
+
+  # statement {
+  #   actions = [
+  #     "iot:Subscribe",
+  #   ]
+  #   resources = [
+  #     "arn:aws:iot:ap-northeast-1:${var.account_id}:topicfilter/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/update/delta",
+  #     "arn:aws:iot:ap-northeast-1:${var.account_id}:topicfilter/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/update/accepted",
+  #     "arn:aws:iot:ap-northeast-1:${var.account_id}:topicfilter/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/update/rejected",
+  #     "arn:aws:iot:ap-northeast-1:${var.account_id}:topicfilter/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/get/accepted",
+  #     "arn:aws:iot:ap-northeast-1:${var.account_id}:topicfilter/$aws/things/&{iot:Connection.Thing.ThingName}/shadow/get/rejected"
+  #   ]
+  # }
 }
 
 resource "aws_iot_policy" "things_policy" {
